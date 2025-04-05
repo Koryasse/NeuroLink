401f08c08b158ef458c077cd7882f12eae5851ce
Koryasse <korbi_yassine@hotmail.com>
2025-04-06 00:11:33 +0200

    Refait toute l'accueil du site

--- NeuroLink/public/js/script.js
+++ NeuroLink/public/js/script.js
@@ -3,3 +3,13 @@ function scrollToBottom() {
     const messagesContainer = document.getElementById('messages-container');
     messagesContainer.scrollTop = messagesContainer.scrollHeight;
 }
+
+let menu = document.getElementById('menu-user-list');
+let button = document.getElementById('menu-user');
+button.addEventListener('click', () => {
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+});
