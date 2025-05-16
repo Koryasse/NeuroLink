01965781a062d900f1d1faa290808a21fae0bbc6
jing752 <theo.httlr@eduge.ch>
2025-05-16 11:00:14 +0200

    black mode and light

--- public/js/script.js
+++ public/js/script.js
@@ -17,35 +17,41 @@ let deco = document.getElementById('disconnection');
 let totalAppareance = document.getElementById('total-change-appareance');
 let totalFurtiv = document.getElementById('total-furtive-mode');
 
+
 // Lorsque l'on clique sur "Retour à l'accueil"
 back.addEventListener('click', () => {
     window.location.href = '../index.html';
 });
-
-// Lorsque l'on clique sur "Changer l'apparance"
-let appBool = true;
-appareance.addEventListener('click', () => {
-    if (appBool) {
-        totalAppareance.innerHTML = 
-            `<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="none" viewBox="0 0 24 24">
-                <path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 21a9 9 0 0 1-.5-17.986V3c-.354.966-.5 1.911-.5 3a9 9 0 0 0 9 9c.239 0 .254.018.488 0A9.004 9.004 0 0 1 12 21Z"/>
+let color = true
+function changer()
+{
+    
+    if(color)
+    {
+        document.body.style.backgroundColor = "#1c1b1f"
+        mess.style.backgroundColor = "#1c1b1f"
+        
+        document.body.style.color = "white";
+        document.getElementById("message-input").style.backgroundColor = "#1c1b1f";
+        document.getElementById("message-input").style.color="#ffffff"
+        document.getElementById("file").innerHTML = `
+            <svg class="w-[25px] h-[25px]" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.5 8H4m0-2v13a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1h-5.032a1 1 0 0 1-.768-.36l-1.9-2.28a1 1 0 0 0-.768-.36H5a1 1 0 0 0-1 1Z"/>
             </svg>`;
-            appBool = false;
-    } else {
-        totalAppareance.innerHTML = 
-            `<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="none" viewBox="0 0 24 24">
-                <path stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5V3m0 18v-2M7.05 7.05 5.636 5.636m12.728 12.728L16.95 16.95M5 12H3m18 0h-2M7.05 16.95l-1.414 1.414M18.364 5.636 16.95 7.05M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z"/>
-            </svg>`;
-            appBool = true;
+        color= false
     }
-});
-
-// Lorsque l'on clique sur "Mode furtif"
-let furtivBool = true;
-furtif.addEventListener('click', () => {
+    else
+    {
+        document.body.style.backgroundColor = "#ffffff";
+        mess.style.backgroundColor = "#ffffff";
+        document.body.style.color = "black";
+        document.getElementById("message-input").style.backgroundColor = "#ffffff";
+        document.getElementById("message-input").style.color = "#000000";
+        document.getElementById("file").innerHTML = `
+        <svg class="w-[25px] h-[25px]" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.5 8H4m0-2v13a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1h-5.032a1 1 0 0 1-.768-.36l-1.9-2.28a1 1 0 0 0-.768-.36H5a1 1 0 0 0-1 1Z"/>
+        </svg>`;
+        color=true
+    }
     
-});
-
-deco.addEventListener('click', () => {
-    
-});
+}
